87103960d4719d4626f216c54be272a521f6e4f6
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2020-04-25 15:33:48 +0700

    make it better

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 import fetch from  'isomorphic-fetch';
 
 // const BASE_API_URL = "http://192.168.1.5:5000/"
-const BASE_API_URL = "http://6428d98b.ngrok.io:4040/"
+const BASE_API_URL = "http://7c774719.ngrok.io:4040/"
  
 export function api(api_end_point, data) {
  
