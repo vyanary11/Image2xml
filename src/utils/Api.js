150503eaf43d8ba9062243c1f30fa21a699649b8
Vyan Ary Pratama <vyanaryprabowo9763@gmail.com>
2020-05-27 06:01:25 +0700

    committed:   src/utils/Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 import fetch from  'isomorphic-fetch';
 
 // const BASE_API_URL = "http://192.168.1.5:5000/"
-const BASE_API_URL = "http://ac9d4a12.ngrok.io/"
+const BASE_API_URL = "http://552155c2.ngrok.io/"
  
 export function api(api_end_point, data) {
  
